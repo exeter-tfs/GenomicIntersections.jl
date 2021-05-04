848f267cd0e09b623a8181a2ce3e4732fa5d72ed
owensnick <owensnick@gmail.com>
2021-05-04 13:24:15 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/GenomicIntersections.jl
@@ -0,0 +1,5 @@
+module GenomicIntersections
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GenomicIntersections
+using Test
+
+@testset "GenomicIntersections.jl" begin
+    # Write your tests here.
+end
